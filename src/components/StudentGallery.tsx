d41b89dd4489cf78f8e55b66196cec55b9273f76
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-11-22 11:59:29 +0100

    Add multiple conditional renders

--- src/components/StudentGallery.tsx
+++ src/components/StudentGallery.tsx
@@ -18,13 +18,38 @@ export default function StudentGallery(props: StudentGalleryProps) {
         setSearchText(event.target.value)
     }
 
+    function mapStudents() {
+        if (filteredStudents.length > 0) {
+            return filteredStudents.map(student => <StudentCard student={student} key={student.id}
+                                                                removeStudent={props.removeStudent}/>)
+        }
+        return <p>Es wurden keine Student gefunden!</p>
+    }
+
     return (
         <div>
-            <input className={"student-gallery__search"} placeholder={"Search"} value={searchText} onChange={onSearchChange}/>
+            <input className={"student-gallery__search"} placeholder={"Search"} value={searchText}
+                   onChange={onSearchChange}/>
 
             <div className="student-gallery">
-                {filteredStudents.map(student => <StudentCard student={student} key={student.id}
-                                                              removeStudent={props.removeStudent}/>)}
+                {filteredStudents.length > 0 && filteredStudents.map(student => <StudentCard student={student}
+                                                                                             key={student.id}
+                                                                                             removeStudent={props.removeStudent}/>)}
+                {filteredStudents.length <= 0 && <p>Es wurden keine Student gefunden!</p>}
+
+            </div>
+
+            <div className="student-gallery">
+                {mapStudents()}
+            </div>
+
+            <div className="student-gallery">
+
+                {filteredStudents.length > 0
+                    ? filteredStudents.map(student => <StudentCard student={student} key={student.id}
+                                                                   removeStudent={props.removeStudent}/>)
+                    : <p>Es wurden keine Student gefunden!</p>
+                }
             </div>
         </div>
     )
