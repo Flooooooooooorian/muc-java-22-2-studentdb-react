398da64586376bcb0bb0d81f9994c858b9dad21e
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-11-22 10:11:06 +0100

    Add useStudents Hook

--- src/components/StudentApp.tsx
+++ src/components/StudentApp.tsx
@@ -1,48 +1,16 @@
-import {useEffect, useState} from "react";
-import {NewStudent, Student} from "../model/Student";
-import axios from "axios";
 import StudentGallery from "./StudentGallery";
 import AddStudent from "./AddStudent";
 import "./StudentApp.css"
+import useStudents from "../hooks/useStudents";
 
 export default function StudentApp() {
 
-
-    const [students, setStudents] = useState<Student[]>([])
-
-    useEffect(() => {
-        getStudents()
-    }, [])
-
-    function getStudents() {
-        axios.get("/student")
-            .then(response => {
-                setStudents(response.data)
-            })
-            .catch(console.error)
-    }
-
-    function addStudent(newStudent: NewStudent) {
-        return axios.post("/student", newStudent)
-            .then(response => response.data)
-            .then((savedStudent) => setStudents(prevState => [...prevState, savedStudent]))
-            .catch(console.error)
-    }
-
-    function removeStudent(id: string) {
-        axios.delete(`/student/${id}`)
-            .then(() => {
-                setStudents(prevState => {
-                    return prevState.filter((student) => student.id !== id
-                    )
-                })
-            })
-    }
+    const [students, addStudents, removeStudent] = useStudents()
 
     return (
         <div className={"student-app"}>
             <h1 className={"student-app__title"}>Students</h1>
-            <AddStudent addStudent={addStudent}/>
+            <AddStudent addStudent={addStudents}/>
             <StudentGallery students={students} removeStudent={removeStudent}/>
         </div>
     )
